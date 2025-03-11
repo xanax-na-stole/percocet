2f811ec18713e897e90f17ebf6ded52af659e9fa
xanax-na-stole <vladislavgumela07@gmail.com>
2025-03-11 23:39:47 +0200

    dal zazu

--- Untitled-3.py
+++ Untitled-3.py
@@ -1,5 +1,9 @@
-print("xanax\nmdma\npercocet\nlirika\nmetadon")
-text = "писать в tg \"ilvzht\""
-print(text)
-text1 = "drugs"
-print(text1*40)
+f = open("1.txt", "w")
+f.write("glaZirOVani pisunczki")
+try:
+  a = int(input())
+  b = int(input())
+  print(a/b)
+except ZeroDivisionError:
+  print("Дура йобана на 0 ділити нізя!") 
+ 
