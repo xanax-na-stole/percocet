abe80b993a1c1153c0960d7d2a5c76932ba0f1ac
xanax-na-stole <vladislavgumela07@gmail.com>
2025-03-23 20:22:00 +0200

    fooeborebo

--- name = 'Каземир'.py
+++ name = 'Каземир'.py
@@ -2,5 +2,6 @@ name = 'Каземир'
 mid_name = 'Залупенко'
 balance = 1941
 
-text = f"""Дорогой {name} {mid_name}, у вас есть задолжность которая составляет {balance} шекелей"""
-print(text)
+text = f"""Дорогой {name} {mid_name}, у вас есть задолжность которая составляет {balance} шекелей."""
+text1 = f""" Huesos beri zaymy u jeny"""
+print(text, text1)
